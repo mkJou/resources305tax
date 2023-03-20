7d6e7c35c96b9385c35b37f72a95e1e3bb8fd468
Joalex Urdaneta <joalexint@gmail.com>
2023-03-20 18:50:47 -0400

    Update chunkreview.js

--- chunkreview.js
+++ chunkreview.js
@@ -3,9 +3,4 @@ const raintgs = document.getElementsByName("rate");
 const feedbackCell = document.getElementById("feedback");
 const textReviewCopy = document.getElementById("textReviewCopy");
 
-feedbackCell.onkeyup = function (e) {
-  if (e.target.value.length < 9) {
-    console.log("jujuj");
-  }
-  console.log("xd");
-};
+alert("Funciona");
